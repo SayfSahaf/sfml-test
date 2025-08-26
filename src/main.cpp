26a0f5cefd063f913c3a9aeb81bf89a6259aa17e
saysah <sayf.sahaf@gmail.com>
2025-08-26 13:10:28 -0400

    rearrage source files into approriate directories. sfml works

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "SFML Works");
     window.setFramerateLimit(144);
 
     while (window.isOpen())
@@ -18,10 +18,11 @@ int main()
         sf::CircleShape c1(50.0f);
         c1.setFillColor(sf::Color(100, 50, 170));
 
-        window.clear(sf::Color::White);
-        window.draw(
-            c1
-        );
+        sf::CircleShape c2(100.0f);
+        c2.setFillColor(sf::Color(70, 200, 90));
+
+        window.clear(sf::Color::Black);
+        window.draw(c2);
         window.display();
     }
 }
